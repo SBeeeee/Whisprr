f45ef4f95ce3bb9f10fc753295429a92bc24ba63
SBeeeee <shatadru017@gmail.com>
2026-01-03 00:05:57 +0530

    added typo fix

--- backend/src/routes/reminder.routes.js
+++ backend/src/routes/reminder.routes.js
@@ -6,7 +6,7 @@ import { verifyUser } from "../middlewares/auth.middleware.js";
 const router = express.Router();
 
 router.post("/",verifyUser, createReminders);
-router.get("/", async (req, res) => {
+router.get("/1", async (req, res) => {
     try {
       await scheduleReminders(); // Trigger the reminder logic
       res.status(200).send("Reminders checked and sent (if any)");

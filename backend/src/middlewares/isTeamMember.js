0b6612ba7696795e5ead4c5667879e577c6044c7
SBeeeee <shatadru017@gmail.com>
2026-01-05 22:56:06 +0530

    added middleware fix

--- backend/src/middlewares/isTeamMember.js
+++ backend/src/middlewares/isTeamMember.js
@@ -1,19 +1,19 @@
-import Team from "../models/Team.models.js";
-
 export const isTeamMember = async (req, res, next) => {
-    const teamId = req.params.teamId
-  
-    if (!teamId) return next(); // personal task
-  
-    const team = await Team.findById(teamId);
-    if (!team) return res.status(404).json({ message: "Team not found" });
-  
-    const member = team.members.find(m => m.user.equals(req.id));
-    if (!member)
-      return res.status(403).json({ message: "Not a team member" });
-  
-    req.team = team;
-    req.teamRole = member.role;
-    next();
-  };
-  
+  const teamId =
+    req.params.teamId ||
+    req.body.team ||
+    req.query.team;
+
+  if (!teamId) return next(); // personal task
+
+  const team = await Team.findById(teamId);
+  if (!team) return res.status(404).json({ message: "Team not found" });
+
+  const member = team.members.find(m => m.user.equals(req.id));
+  if (!member)
+    return res.status(403).json({ message: "Not a team member" });
+
+  req.team = team;
+  req.teamRole = member.role;
+  next();
+};
